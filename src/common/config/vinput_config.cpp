8e49f85e150ea6f62abb60041e9d9336424fff34
Yifan Zhu <fanzhuyifan@gmail.com>
2026-09-06 19:40:14 -0700

    feat(config): make hold activation delay configurable
    Add the bounded hold-delay setting, help text, and Chinese translations.

--- src/common/config/vinput_config.cpp
+++ src/common/config/vinput_config.cpp
@@ -1,5 +1,6 @@
 #include "common/config/vinput_config.h"
 
+#include <fcitx-config/option.h>
 #include <fcitx-utils/keysymgen.h>
 #include <fcitx-utils/standardpath.h>
 #include <filesystem>
@@ -103,6 +104,13 @@ std::string MaxStreamingDisplayWidthTooltip() {
 VinputConfig::VinputConfig()
     : triggerMode(this, "TriggerMode", TriggerModeLabel(), TriggerMode::Both, {}, {},
                   TriggerModeI18NAnnotation()),
+      holdActivationDelay(
+          this, "HoldActivationDelay", _("Hold activation delay (ms)"), 300,
+          fcitx::IntConstrain(100, 2000), {},
+          fcitx::ToolTipAnnotation(
+              _("Delay before recording in Hold mode and the tap/hold boundary in Both mode. "
+                "In Both mode, non-modifier shortcuts still start recording immediately. "
+                "Tap mode ignores this setting."))),
       triggerKeys(this, "TriggerKey", TriggerKeyLabel(), {fcitx::Key(FcitxKey_Alt_R)},
                   TriggerKeyListConstrain(), {}, fcitx::ToolTipAnnotation(TriggerKeyTooltip())),
       commandKeys(this, "CommandKeys", CommandKeysLabel(), {fcitx::Key(FcitxKey_Control_R)},
